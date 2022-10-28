d1d91b3389e43e0d20f2e43ce43d8e28f5d168ec
ginofrigoni <ginofrigoni@gmail.com>
2022-10-28 15:57:21 -0300

    Implemento un template bootstrap basico.

--- articulo/views.py
+++ articulo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    data = {
+        'titulo' : 'Titulo este es el',
+    }
+    return render(request, 'index.html', data)
+
+def ver(request, nombre_del_art):
+    data = {
+        'titulo' : nombre_del_art,
+    }
+    return render(request, 'articulo.html', data)
+
+def about(request):
+    data = {
+        'titulo' : 'about',
+    }
+    return render(request, 'articulo.html', data)
--- principal/urls.py
+++ principal/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from articulo.views import ver
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path('', include('articulo.urls')),
+    path('admin/', admin.site.urls),   
+    path('<nombre_del_art>/', ver),
 ]
 #comentario
